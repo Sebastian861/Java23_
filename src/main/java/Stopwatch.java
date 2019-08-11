d45e70c13023ac5fbcd8954d412ba0c4cf94ca1e
Sebastian Gwoździk <sebastian.gwozdzik@o2.pl>
2019-08-11 12:32:13 +0200

    Pair

---
+++ src/main/java/Stopwatch.java
@@ -0,0 +1,10 @@
+public class Stoper {
+    long startTime= System.nanoTime();
+    long stopTime = System.nanoTime();
+    long totalTime = stopTime-startTime;
+
+public long setStartTime() {
+    return
+}
+
+}
